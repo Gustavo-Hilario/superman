9854b66769b88258d47d8bcad460ea11fc21580d
Gustavo_H <gustavo.hilario@automattic.com>
2022-11-07 21:54:56 -0500

    Refactoring site's header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,94 +8,103 @@ import { connect } from "react-redux";
 import { fetchUserInfo } from "../actions";
 
 class Header extends React.Component {
-    renderHeaderUser = () => {
+    // Helper functions to Render the entire header with renderHeader()
+    renderBranding = () => {
         if (this.props.userInfo === null) {
-            return (
-                <div className="row align-items-center">
-                    <div className="col-md-6">
-                        <div className="site-branding d-flex justify-content-center align-items-center mb-3">
-                            <div className="site-logo">
-                                <Link to="/">
-                                    <img
-                                        src="/images/superman.png"
-                                        style={{
-                                            width: "100px",
-                                            height: "100px",
-                                        }}
-                                    />
-                                </Link>
-                            </div>
-                            <div className="">
-                                <div className="site-title fw-bolder">
-                                    <Link to="/">Superman</Link>
-                                </div>
-                                <div className="site-tagline fs-6 fw-semibold">
-                                    This is my awesome site!
-                                </div>
-                            </div>
+            var col = "col-md-6";
+        } else {
+            var col = "col-md-3";
+        }
+        return (
+            <div className={`${col} site-branding`}>
+                <div className="d-flex justify-content-center align-items-center mb-3">
+                    <div className="site-logo">
+                        <Link to="/">
+                            <img
+                                src="/images/superman.png"
+                                style={{
+                                    width: "100px",
+                                    height: "100px",
+                                }}
+                            />
+                        </Link>
+                    </div>
+                    <div className="">
+                        <div className="site-title fw-bolder">
+                            <Link to="/">Superman</Link>
+                        </div>
+                        <div className="site-tagline fs-6 fw-semibold">
+                            This is my awesome site!
                         </div>
                     </div>
-                    <div className="site-menu col-md-6 d-flex justify-content-center">
-                        <Link to="/" className="pe-3">
-                            Home
-                        </Link>
-                        <Link to="/wpcom" className="pe-3">
-                            WordPress.com
-                        </Link>
-                        <GoogleLog />
-                    </div>
+                </div>
+            </div>
+        );
+    };
+
+    // This could use some enhacement. I'm not sure how I could conditionally add the <GoogleLog> component in the menu
+    renderMenu = () => {
+        if (this.props.userInfo === null) {
+            return (
+                <div className="site-menu col-md-6 d-flex justify-content-center">
+                    <Link to="/" className="pe-3">
+                        Home
+                    </Link>
+                    <Link to="/wpcom" className="pe-3">
+                        WordPress.com
+                    </Link>
+                    <GoogleLog />
                 </div>
             );
         } else {
             return (
-                <div className="row align-items-center">
-                    <div className="col-md-3">
-                        <div className="site-branding d-flex justify-content-center align-items-center mb-3">
-                            <div className="site-logo">
-                                <img
-                                    src="/images/superman.png"
-                                    style={{ width: "100px", height: "100px" }}
-                                />
-                            </div>
-                            <div className="">
-                                <div className="site-title fw-bolder">
-                                    <Link to="/">Superman</Link>
-                                </div>
-                                <div className="site-tagline fs-6 fw-semibold">
-                                    This is my awesome site!
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="site-menu col-md-6 d-flex justify-content-center">
-                        <Link to="/" className="pe-3">
-                            Home
-                        </Link>
-                        <Link to="/wpcom" className="pe-3">
-                            WordPress.com
-                        </Link>
-                    </div>
-                    <div className="col-md-3 d-flex justify-content-center align-items-center gap-3 ">
-                        {/* Getting Google Profile image from Google! It was necessary to add the referrerpolicy property. I was getting as 403 error */}
-                        <Link to="/my-account">
-                            <p className="mb-0 fw-semibold">
-                                {this.props.userInfo.UserName.split(" ")[0]}
-                            </p>
-                        </Link>
-                        <Link to="/my-account">
-                            <img
-                                src={this.props.userInfo.UserProfilePhoto}
-                                referrerPolicy="no-referrer"
-                                className="rounded-circle profile-img"
-                                width="48px"
-                                height="48px"
-                            />
-                        </Link>
-                        <GoogleLog />
-                    </div>
+                <div className="site-menu col-md-6 d-flex justify-content-center">
+                    <Link to="/" className="pe-3">
+                        Home
+                    </Link>
+                    <Link to="/wpcom" className="pe-3">
+                        WordPress.com
+                    </Link>
                 </div>
             );
         }
+    };
+
+    renderMyAccount = () => {
+        if (this.props.userInfo === null) {
+            return;
+        } else {
+            return (
+                <div className="my-account-header col-md-3 d-flex justify-content-center align-items-center gap-3 ">
+                    {/* Getting Google Profile image from Google! It was necessary to add the referrerpolicy property. I was getting as 403 error */}
+                    <Link to="/my-account">
+                        <p className="mb-0 fw-semibold">
+                            {this.props.userInfo.UserName.split(" ")[0]}
+                        </p>
+                    </Link>
+                    <Link to="/my-account">
+                        <img
+                            src={this.props.userInfo.UserProfilePhoto}
+                            referrerPolicy="no-referrer"
+                            className="rounded-circle profile-img"
+                            width="48px"
+                            height="48px"
+                        />
+                    </Link>
+                    <GoogleLog />
+                </div>
+            );
+        }
+    };
+
+    renderHeaderUser = () => {
+        return (
+            <div className="row align-items-center">
+                {this.renderBranding()}
+                {this.renderMenu()}
+                {this.renderMyAccount()}
+            </div>
+        );
     };
 
     render() {
---
+++ src/components/YoutubeList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class YoutubeList extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>YoutubeList</p>
+            </div>
+        );
+    }
+}
+
+export default YoutubeList;

6a6d4354337941509172f844aafb3a3bdf259ad8
Gustavo_H <gustavo.hilario@automattic.com>
2022-11-21 07:45:07 -0500

    Stuck on validating and submitting React Form

--- src/components/ListForm.js
+++ src/components/ListForm.js
@@ -1,16 +1,20 @@
 import React from "react";
 
 // Redux Form
-
+// By importing this some props are added to the component including HandleSubmit
 import { reduxForm, Field } from "redux-form";
 
 class ListForm extends React.Component {
+    componentDidMount() {
+        console.log(this.props);
+    }
+
     // Rendering the Components that will replace the Field Components
     renderFieldInput = ({ input, label, meta, type, id }) => {
         const className = {
             container: "mb-3",
             label: "form-label fw-bolder",
-            input: "form-control",
+            input: "form-control form-control-lg",
         };
 
         var renderFieldType = (className, type, id) => {
@@ -35,14 +39,43 @@ class ListForm extends React.Component {
             }
         };
 
+        var renderError = (meta) => {
+            if (meta.touched && meta.error) {
+                return (
+                    <div className="field-error-message">
+                        <div className="">{meta.error}</div>
+                    </div>
+                );
+            }
+        };
+
+        // renderError({ error, touched }) {
+        //     if (touched && error) {
+        //         return (
+        //             <div className="ui error message">
+        //                 <div className="header">{error}</div>
+        //             </div>
+        //         );
+        //     }
+        // }
+
         return (
             <div className={className.container}>
                 <label className={className.label} htmlFor={id}>
                     {label}
                 </label>
                 {renderFieldType(className, type, id)}
+                {renderError(meta)}
             </div>
         );
+    };
+
+    onFormSubmit = (formValues) => {
+        // redux-form helps making sure the code below runs and no event handling would be necessary
+        // e.preventDefault();
+
+        console.log(formValues);
+        this.props.submit(formValues);
     };
 
     render() {
@@ -50,21 +83,27 @@ class ListForm extends React.Component {
             <div>
                 <h3 className="text-center">ListForm Component</h3>
                 <div className="d-flex justify-content-center">
-                    <form className="w-75">
+                    {/* handleSubmit is a function/props that comes from the redux-form library */}
+
+                    <form
+                        className="w-75"
+                        onSubmit={this.props.handleSubmit(this.onFormSubmit)}
+                    >
                         <Field
-                            name="note-subject"
+                            name="noteSubject"
                             component={this.renderFieldInput}
                             label="Subject"
                             type="text"
                             id="my-field"
+                            placeholder="First Name"
                         />
 
                         <Field
-                            name="note-content"
+                            name="noteContent"
                             component={this.renderFieldInput}
                             label="Content"
                             type="textarea"
-                            id="note-content"
+                            id="noteContent"
                         />
                         <button type="submit" className="btn btn-primary">
                             Submit
@@ -76,4 +115,19 @@ class ListForm extends React.Component {
     }
 }
 
-export default reduxForm({ form: "listForm" })(ListForm);
+const validate = (formValues) => {
+    // console.log(formValues);
+    const errors = {};
+
+    if (!formValues.noteSubject) {
+        errors.title = "You must enter a Subject";
+    }
+
+    if (!formValues.noteContent) {
+        errors.title = "Enter the content!!";
+    }
+
+    return console.log(errors);
+};
+
+export default reduxForm({ form: "listForm", validate })(ListForm);
